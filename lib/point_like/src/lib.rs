73169c753c41c2c8489f9d34cedbfb35604618d2
cuppachino <cuppachino.dev@gmail.com>
2023-12-05 16:54:25 -0600

    Add new files and dependencies

---
+++ lib/point_like/src/lib.rs
@@ -0,0 +1,348 @@
+extern crate num;
+use num::Float;
+
+/// A number that may be compared for approximate equality.
+pub trait ApproxEq<Rhs = Self, Epsilon = Self> {
+    fn approx_eq(&self, rhs: Rhs, epsilon: Epsilon) -> bool;
+}
+
+impl<F> ApproxEq<Self, Self> for F
+where
+    F: Float,
+{
+    fn approx_eq(&self, rhs: Self, epsilon: Self) -> bool {
+        (self.sub(rhs)).abs() < epsilon
+    }
+}
+
+/// A trait for rounding scalars.
+pub trait PrecisionRound {
+    /// Precision round a scalar to a number of decimal places.
+    fn round(self, precision: usize) -> Self;
+}
+
+impl<F: Float> PrecisionRound for F {
+    /// Round a scalar to `precision` decimal places.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the precision factor cannot be represented by `F`.
+    #[inline]
+    fn round(self, precision: usize) -> Self {
+        let factor = F::from(10.0_f32.powi(precision as i32)).unwrap();
+        (self * factor).round() / factor
+    }
+}
+
+/// A point-like type that can be converted to and from `[F; 2]`.
+///
+/// Any third component is assumed to be zero.
+pub trait FromInto<T>: Sized {
+    fn from_point_like(t: T) -> Self;
+    fn into_point_like(self) -> T;
+}
+
+impl<T> FromInto<T> for T
+where
+    Self: From<T> + Into<T>,
+{
+    #[inline]
+    fn from_point_like(t: T) -> Self {
+        t.into()
+    }
+    #[inline]
+    fn into_point_like(self) -> T {
+        self.into()
+    }
+}
+
+impl<F: Float> FromInto<[F; 2]> for [F; 3] {
+    #[inline]
+    fn from_point_like(t: [F; 2]) -> Self {
+        [t[0], t[1], F::zero()]
+    }
+    #[inline]
+    fn into_point_like(self) -> [F; 2] {
+        [self[0], self[1]]
+    }
+}
+
+impl<F: Float> FromInto<[F; 2]> for (F, F) {
+    #[inline]
+    fn from_point_like(t: [F; 2]) -> Self {
+        (t[0], t[1])
+    }
+    #[inline]
+    fn into_point_like(self) -> [F; 2] {
+        [self.0, self.1]
+    }
+}
+
+impl<F: Float> FromInto<[F; 2]> for (F, F, F) {
+    #[inline]
+    fn from_point_like(t: [F; 2]) -> Self {
+        (t[0], t[1], F::zero())
+    }
+    #[inline]
+    fn into_point_like(self) -> [F; 2] {
+        [self.0, self.1]
+    }
+}
+
+/// A point-like type that can be used to define a coordinate in 2D space.
+pub trait PointLike<N: Float>: Copy + PartialEq + FromInto<[N; 2]> {
+    fn x(&self) -> N;
+    fn y(&self) -> N;
+    fn with_x(&self, x: N) -> Self;
+    fn with_y(&self, y: N) -> Self;
+    fn with_xy(x: N, y: N) -> Self;
+}
+
+impl<F: Float> PointLike<F> for [F; 2] {
+    #[inline]
+    fn x(&self) -> F {
+        self[0]
+    }
+    #[inline]
+    fn y(&self) -> F {
+        self[1]
+    }
+    #[inline]
+    fn with_x(&self, x: F) -> Self {
+        [x, self.y()]
+    }
+    #[inline]
+    fn with_y(&self, y: F) -> Self {
+        [self.x(), y]
+    }
+    #[inline]
+    fn with_xy(x: F, y: F) -> Self {
+        [x, y]
+    }
+}
+
+impl<F: Float> PointLike<F> for (F, F) {
+    #[inline]
+    fn x(&self) -> F {
+        self.0
+    }
+    #[inline]
+    fn y(&self) -> F {
+        self.1
+    }
+    #[inline]
+    fn with_x(&self, x: F) -> Self {
+        (x, self.y())
+    }
+    #[inline]
+    fn with_y(&self, y: F) -> Self {
+        (self.x(), y)
+    }
+    #[inline]
+    fn with_xy(x: F, y: F) -> Self {
+        (x, y)
+    }
+}
+
+impl<F: Float> PointLike<F> for (F, F, F) {
+    #[inline]
+    fn x(&self) -> F {
+        self.0
+    }
+    #[inline]
+    fn y(&self) -> F {
+        self.1
+    }
+    #[inline]
+    fn with_x(&self, x: F) -> Self {
+        (x, self.y(), self.2)
+    }
+    #[inline]
+    fn with_y(&self, y: F) -> Self {
+        (self.x(), y, self.2)
+    }
+    #[inline]
+    fn with_xy(x: F, y: F) -> Self {
+        (x, y, F::zero())
+    }
+}
+
+pub trait PointOps<F: Float> {
+    fn add(self, rhs: Self) -> Self;
+
+    fn sub(self, rhs: Self) -> Self;
+
+    fn mul(self, rhs: Self) -> Self;
+}
+
+impl<F: Float, P: PointLike<F>> PointOps<F> for P {
+    #[inline]
+    fn add(self, other: Self) -> Self {
+        Self::from_point_like([self.x() + other.x(), self.y() + other.y()])
+    }
+
+    #[inline]
+    fn sub(self, other: Self) -> Self {
+        Self::from_point_like([self.x() - other.x(), self.y() - other.y()])
+    }
+
+    #[inline]
+    fn mul(self, rhs: Self) -> Self {
+        Self::from_point_like([self.x() * rhs.x(), self.y() * rhs.y()])
+    }
+}
+
+pub trait PointOpsExt<F: Float>: PointLike<F> {
+    /// Add a scalar to both components of a point.
+    #[inline]
+    fn add_f(self, scalar: F) -> Self {
+        Self::from_point_like([self.x() + scalar, self.y() + scalar])
+    }
+
+    /// Subtract a scalar from both components of a point.
+    #[inline]
+    fn sub_f(self, scalar: F) -> Self {
+        Self::from_point_like([self.x() - scalar, self.y() - scalar])
+    }
+
+    /// Multiply a point by a scalar.
+    #[inline]
+    fn mul_f(self, scalar: F) -> Self {
+        Self::from_point_like([self.x() * scalar, self.y() * scalar])
+    }
+
+    /// Construct a point from the absolute components of self.
+    #[inline]
+    fn abs(self) -> Self {
+        Self::from_point_like([self.x().abs(), self.y().abs()])
+    }
+
+    /// Compare all components in self with `rhs` for approximate equality.
+    #[inline]
+    fn approx_eq(self, rhs: Self, epsilon: F) -> bool {
+        self.sub(rhs).abs().lt(&epsilon)
+    }
+
+    /// Compare all components in a point with a scalar for approximate equality.
+    #[inline]
+    fn approx_eq_f(self, scalar: F, epsilon: F) -> bool {
+        self.sub_f(scalar).abs().lt(&epsilon)
+    }
+
+    /// Round the components of a point to N decimal places.
+
+    #[inline]
+    fn round(self, n: usize) -> Self {
+        let factor = F::from(10).unwrap().powi(n as i32);
+        let x = (self.x() * factor).round() / factor;
+        let y = (self.y() * factor).round() / factor;
+        Self::from_point_like([x, y])
+    }
+
+    /// Returns a point-like containing the maximum values for each element of `self` and `rhs`.
+    ///
+    /// In other words this computes `[self.x().max(rhs.x), self.y().max(rhs.y())]`.
+    #[inline]
+    fn max(self, other: Self) -> Self {
+        Self::from_point_like([self.x().max(other.x()), self.y().max(other.y())])
+    }
+
+    /// Returns a point-like containing the minimum values for each element of `self` and `rhs`.
+    ///
+    /// In other words this computes `[self.x.min(rhs.x), self.y.min(rhs.y)]`.
+    #[inline]
+    fn min(self, other: Self) -> Self {
+        Self::from_point_like([self.x().min(other.x()), self.y().min(other.y())])
+    }
+
+    /// Compute the cross product of two points.
+    #[inline]
+    fn cross(self, rhs: Self) -> F {
+        self.x() * rhs.y() - self.y() * rhs.x()
+    }
+
+    /// Computes the dot of `self` and `rhs`.
+    ///
+    /// ```maths
+    /// a · b = (x₁ · x₂) + (y₁ · y₂)
+    /// ```
+    fn dot(self, rhs: Self) -> F {
+        self.x() * rhs.x() + self.y() * rhs.y()
+    }
+
+    /// Computes the length of `self`.
+    ///
+    /// ```maths
+    /// c = √(a² + b²)
+    /// ```
+    #[doc(alias = "magnitude")]
+    #[inline]
+    fn length(self) -> F {
+        self.dot(self).sqrt()
+    }
+
+    /// Computes the Euclidean distance between two points in space.
+    ///
+    /// This works by relocating one point to the origin,
+    /// then computing the length of the resulting vector.
+    ///
+    /// ```maths
+    /// c = √((x₂ - x₁)² + (y₂ - y₁)²)
+    /// ```
+    #[inline]
+    fn distance(self, rhs: Self) -> F {
+        (self.sub(rhs)).length()
+    }
+
+    #[inline]
+    fn partial_cmp(&self, other: &F) -> Option<std::cmp::Ordering> {
+        let x = self.x().partial_cmp(other);
+        let y = self.y().partial_cmp(other);
+        match (x, y) {
+            (Some(std::cmp::Ordering::Equal), Some(std::cmp::Ordering::Equal)) => {
+                Some(std::cmp::Ordering::Equal)
+            }
+            (Some(std::cmp::Ordering::Equal), _) => y,
+            (_, Some(std::cmp::Ordering::Equal)) => x,
+            (Some(std::cmp::Ordering::Less), Some(std::cmp::Ordering::Less)) => {
+                Some(std::cmp::Ordering::Less)
+            }
+            (Some(std::cmp::Ordering::Greater), Some(std::cmp::Ordering::Greater)) => {
+                Some(std::cmp::Ordering::Greater)
+            }
+            _ => None,
+        }
+    }
+
+    #[inline]
+    fn lt(&self, other: &F) -> bool {
+        self.partial_cmp(other) == Some(std::cmp::Ordering::Less)
+    }
+
+    #[inline]
+    fn gt(self, other: &F) -> bool {
+        self.partial_cmp(other) == Some(std::cmp::Ordering::Greater)
+    }
+
+    #[inline]
+    fn le(self, other: &F) -> bool {
+        self.partial_cmp(other) != Some(std::cmp::Ordering::Greater)
+    }
+
+    #[inline]
+    fn ge(self, other: &F) -> bool {
+        self.partial_cmp(other) != Some(std::cmp::Ordering::Less)
+    }
+
+    #[inline]
+    fn eq(self, other: &F) -> bool {
+        self.partial_cmp(other) == Some(std::cmp::Ordering::Equal)
+    }
+
+    #[inline]
+    fn ne(self, other: &F) -> bool {
+        self.partial_cmp(other) != Some(std::cmp::Ordering::Equal)
+    }
+}
+
+impl<F: Float, P: PointLike<F>> PointOpsExt<F> for P {}
---
+++ src/lib.rs
@@ -0,0 +1,284 @@
+extern crate num;
+extern crate point_like;
+
+use derive_new::new;
+use num::Float;
+use std::marker::PhantomData;
+
+use point_like::*;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum IntersectionResult<F: Float, P: PointLike<F>> {
+    Intersection(Intersection<F, P>),
+    Parallel,
+    None,
+}
+
+impl<F: Float, P: PointLike<F>> IntersectionResult<F, P> {
+    /// Returns `true` if the intersection result is [`Intersection`].
+    ///
+    /// [`Intersection`]: IntersectionResult::Intersection
+    #[must_use]
+    pub fn is_intersection(&self) -> bool {
+        matches!(self, Self::Intersection(..))
+    }
+
+    /// Convert from `IntersectionResult<F, P>` to `Result<Intersection<F, P>, Self>`.
+    ///
+    /// Converts `IntersectionResult::Parallel` or `IntersectionResult::None` to `Err(self)`.
+    pub fn try_into_intersection(self) -> Result<Intersection<F, P>, Self> {
+        if let Self::Intersection(v) = self {
+            Ok(v)
+        } else {
+            Err(self)
+        }
+    }
+
+    /// Convert from `IntersectionResult<F, P>` to `Option<&Intersection<F, P>>`.
+    ///
+    /// Discards `IntersectionResult::Parallel` and `IntersectionResult::None` variants.
+    pub fn as_intersection(&self) -> Option<&Intersection<F, P>> {
+        if let Self::Intersection(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
+impl<F: Float, P: PointLike<F>> PrecisionRound for IntersectionResult<F, P> {
+    /// Round each component in the result of an intersection computation to `precision` decimal places.
+    ///
+    /// This does not operate on `Parallel` or `None` variants.
+    #[inline]
+    fn round(self, precision: usize) -> Self {
+        match self {
+            Self::Intersection(intersection) => Self::Intersection(intersection.round(precision)),
+            Self::Parallel => Self::Parallel,
+            Self::None => Self::None,
+        }
+    }
+}
+
+/// An intersection between two [`Line`].
+#[derive(Debug, PartialEq, new, Clone)]
+pub enum Intersection<F: Float, P: PointLike<F>> {
+    Point(P),
+    CollinearSegment(Line<F, P>),
+    Coincident(Line<F, P>),
+}
+
+impl<F: Float, P: PointLike<F>> PrecisionRound for Intersection<F, P> {
+    /// Round an intersection's components to `precision` decimal places.
+    #[inline]
+    fn round(self, precision: usize) -> Self {
+        match self {
+            Self::Point(p) => Self::Point(p.round(precision)),
+            Self::CollinearSegment(line) => Self::CollinearSegment(line.round(precision)),
+            Self::Coincident(line) => Self::Coincident(line.round(precision)),
+        }
+    }
+}
+
+impl<F: Float, P: PointLike<F>> From<P> for Intersection<F, P> {
+    fn from(point: P) -> Self {
+        Self::Point(point)
+    }
+}
+
+/// A line segment from `start` to `end`.
+///
+/// See: [`PointLike`]
+#[derive(Eq, Debug, new, Clone, Copy)]
+pub struct Line<F: Float + PartialEq, P: PointLike<F>> {
+    pub start: P,
+    pub end: P,
+    #[new(default)]
+    marker: PhantomData<F>,
+}
+
+impl<F: Float, P: PointLike<F>> PartialEq for Line<F, P> {
+    fn eq(&self, other: &Self) -> bool {
+        let epsilon = F::epsilon();
+        self.start.approx_eq(other.start, epsilon) && self.end.approx_eq(other.end, epsilon)
+    }
+}
+
+impl<F: Float, P: PointLike<F>> PrecisionRound for Line<F, P> {
+    /// Round a line segment's start and end points to `precision` decimal places.
+    #[inline]
+    fn round(self, precision: usize) -> Self {
+        Self::new(self.start.round(precision), self.end.round(precision))
+    }
+}
+
+impl<F: Float, P: PointLike<F>> ApproxEq<&Self, F> for Line<F, P> {
+    /// Approximate equality between two line segments.
+    ///
+    /// See: [`PointLike::approx_eq`]
+    #[inline]
+    fn approx_eq(&self, other: &Self, epsilon: F) -> bool {
+        self.start.approx_eq(other.start, epsilon) && self.end.approx_eq(other.end, epsilon)
+    }
+}
+
+impl<F: Float, P: PointLike<F>> Line<F, P> {
+    /// Compute the length of the line segment.
+    #[inline]
+    pub fn length(&self) -> F {
+        self.start.distance(self.end)
+    }
+
+    /// Compute the non-normalized direction of self.
+    #[inline]
+    pub fn direction(&self) -> P {
+        self.end.sub(self.start)
+    }
+
+    /// Compute the intersection between two line segments, if any.
+    pub fn intersection(&self, other: &Self, epsilon: F) -> IntersectionResult<F, P> {
+        if self.approx_eq(&other, epsilon) {
+            return IntersectionResult::Intersection(Intersection::Coincident(*self));
+        }
+        let zero = F::zero();
+        let one = F::one();
+
+        let p = self.start;
+        let r = self.direction();
+        let q = other.start;
+        let s = other.direction();
+        let rs = r.cross(s);
+        let qpr = q.sub(p).cross(r);
+
+        let rs_is_zero = rs.approx_eq(zero, epsilon);
+        let qpr_is_zero = qpr.approx_eq(zero, epsilon);
+        let r_len = r.length();
+        if rs_is_zero && qpr_is_zero && r_len.gt(&zero) {
+            let t1 = q.add(s.sub(p)).dot(r) / r.dot(r);
+            let t0 = t1.sub(s.dot(r) / r_len);
+
+            if t0.ge(&zero) && t0.le(&one) || t1.ge(&zero) && t1.le(&one) {
+                let first_point = self.start.max(other.start);
+                let second_point = self.end.min(other.end);
+                let line = Line::new(first_point, second_point);
+
+                return IntersectionResult::Intersection(Intersection::CollinearSegment(line));
+            }
+        }
+
+        if rs_is_zero && !qpr_is_zero {
+            return IntersectionResult::Parallel;
+        }
+
+        let t = q.sub(p).cross(s) / rs;
+        let u = qpr / rs;
+        if t >= zero && t <= one && u >= zero && u <= one {
+            let intersection = self.start.add(r.mul_f(t));
+            return IntersectionResult::Intersection(Intersection::Point(intersection));
+        }
+
+        IntersectionResult::None
+    }
+}
+
+impl<F: Float, P: PointLike<F>> From<(P, P)> for Line<F, P> {
+    fn from((start, end): (P, P)) -> Self {
+        Self::new(start, end)
+    }
+}
+
+impl<F: Float, P: PointLike<F>> From<[P; 2]> for Line<F, P> {
+    fn from([start, end]: [P; 2]) -> Self {
+        Self::new(start, end)
+    }
+}
+
+impl<F: Float, P: PointLike<F>> From<(&P, &P)> for Line<F, P>
+where
+    P: Clone,
+{
+    fn from((start, end): (&P, &P)) -> Self {
+        Self::new(start.to_owned(), end.to_owned())
+    }
+}
+
+impl<F: Float, P: PointLike<F>> From<[&P; 2]> for Line<F, P>
+where
+    P: Clone,
+{
+    fn from([start, end]: [&P; 2]) -> Self {
+        Self::new(start.to_owned(), end.to_owned())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    // 🙈 NONE
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [0.0_f32, 1.0_f32]),
+        Line::new([0.5_f32, 0.0_f32], [1.0_f32, 0.0_f32]),
+        IntersectionResult::None,
+    )]
+    // ➕ PERPENDICULAR 1:1
+    #[case(
+        Line::new([0.0_f32, 0.5_f32], [1.0_f32, 0.5_f32]), // —
+        Line::new([0.0_f32, 0.0_f32], [1.0_f32, 1.0_f32]), // |
+        IntersectionResult::Intersection(Intersection::Point([0.5_f32, 0.5_f32]))
+    )]
+    // ✖️ CROSSING (1:1)
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [1.0_f32, 1.0_f32]),
+        Line::new([0.0_f32, 1.0_f32], [1.0_f32, 0.0_f32]),
+        IntersectionResult::Intersection(Intersection::Point([0.5_f32, 0.5_f32]))
+    )]
+    // ✖️ CROSSING (ASYMMETRIC)
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [5.0_f32, 5.0_f32]),
+        Line::new([0.0_f32, 10.0_f32], [3.0_f32, 0.0_f32]),
+        IntersectionResult::Intersection(Intersection::Point([2.308_f32, 2.308_f32]))
+    )]
+    // ┃ COINCIDENT
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [0.0_f32, 1.0_f32]),
+        Line::new([0.0_f32, 0.0_f32], [0.0_f32, 1.0_f32]),
+        IntersectionResult::Intersection(Intersection::Coincident(Line::new([0.0_f32, 0.0_f32], [0.0_f32, 1.0_f32])))
+    )]
+    // ╿ COLLINEAR SEGMENT (VERTICAL)
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [0.0_f32, 1.0_f32]),
+        Line::new([0.0_f32, 0.5_f32], [0.0_f32, 1.5_f32]),
+        IntersectionResult::Intersection(Intersection::CollinearSegment(Line::new([0.0_f32, 0.5_f32], [0.0_f32, 1.0_f32])))
+    )]
+    // ╿ COLLINEAR SEGMENT (DIAGONAL)
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [1.0_f32, 1.0_f32]),
+        Line::new([0.5_f32, 0.5_f32], [1.5_f32, 1.5_f32]),
+        IntersectionResult::Intersection(Intersection::CollinearSegment(Line::new([0.5_f32, 0.5_f32], [1.0_f32, 1.0_f32])))
+    )]
+    // ╻┃ PARALLEL (VERTICAL, ASYMMETRIC LEN)
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [0.0_f32, 1.0_f32]),
+        Line::new([1.0_f32, 0.0_f32], [1.0_f32, 2.0_f32]),
+        IntersectionResult::Parallel
+    )]
+    // ╱╱ PARALLEL (DIAGONAL, SYMMETRIC LEN)
+    #[case(
+        Line::new([0.0_f32, 0.0_f32], [1.0_f32, 1.0_f32]),
+        Line::new([0.0_f32, 2.0_f32], [1.0_f32, 3.0_f32]),
+        IntersectionResult::Parallel
+    )]
+
+    fn test_intersections(
+        #[case] line1: Line<f32, [f32; 2]>,
+        #[case] line2: Line<f32, [f32; 2]>,
+        #[case] expected: IntersectionResult<f32, [f32; 2]>,
+    ) {
+        // Round for use with assert_eq!.
+        let result = line1.intersection(&line2, 0.001).round(3);
+        assert_eq!(result, expected);
+    }
+}
